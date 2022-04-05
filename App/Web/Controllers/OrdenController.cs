15b78db4cc88c0fa017dd15ce58c774275535d48
Psiquix <josaza2114@gmail.com>
2022-04-04 22:24:18 -0600

    cambios visuales

--- App/Web/Controllers/OrdenController.cs
+++ App/Web/Controllers/OrdenController.cs
@@ -86,7 +86,6 @@ namespace Web.Controllers
 
         }
 
-        [HttpPost]
         public ActionResult Cancel()
         {
             Carrito.Instancia.eliminarCarrito();
--- App/Web/Utils/SweetAlertHelper.cs
+++ App/Web/Utils/SweetAlertHelper.cs
@@ -12,11 +12,6 @@ namespace Web.Util
             {
                 return "swal({title: '" + titulo + "',text: '" + mensaje + "',type: '" + tipoMensaje + "'});";
             }
-        public static string Mensajea(string titulo, string mensaje, SweetAlertMessageType tipoMensaje)
-            {
-                return "swal({title: '" + titulo + "',text: '" + mensaje + "',type: '" + tipoMensaje + "'});";
-            }
-
     }
     public enum SweetAlertMessageType
     {
